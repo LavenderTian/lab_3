37f9e0f7b810e30b5208f22473b9fa7e69b34f52
LavenderTian <yuti3188@colorado.edu>
2020-02-13 17:23:23 -0700

    1234556666

---
+++ lab3_script.sh
@@ -0,0 +1,29 @@
+#i!/bin/bash
+# Authors : Your Name & Your partner's name
+# Date: 1/1/2019
+
+#Problem 1 Code:
+
+
+
+#Problem 1 Code:
+echo "give a file name"
+read filename
+
+echo "give me grep command"
+read grepcommand
+
+
+grep $grepcommand $filename
+echo "the phone number count is :"
+grep -c "[0-9][0-9][0-9]-" regex_practice.txt
+
+echo "the email count is :"
+grep -c "@" regex_practice.txt
+
+echo "The phone numbers that start with 303 are:"
+grep -o "303-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]" regex_practice.txt 
+
+echo "The email include geocities.com and will be stored in email_results.txt:"
+grep "geocities.com" regex_practice.txt
+grep "geocities.com" regex_practice.txt >>email_results.txt
